988473d79638e79f7b57b2ac81c10a0e33dd6369
Fju <flowi@outlook.com>
2021-11-06 10:07:44 +0100

    Added initial Unity project files

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TimerText timerTextObject;
+    private float gameTime;
+    private int gameSeconds;
+
+    public void Start()
+    {
+        gameTime = 0;
+        gameSeconds = 15;
+    }
+
+    public void Update()
+    {
+        
+        gameTime += Time.deltaTime;
+        if (gameTime > 1.0f)
+        {
+            gameTime -= 1.0f;
+            gameSeconds--;
+            timerTextObject.updateTimer(gameSeconds / 60, gameSeconds % 60);
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/TimerText.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerText : MonoBehaviour
+{
+    public void updateTimer(int minutes, int seconds)
+    {
+        // get textmeshpro object
+        TextMeshProUGUI tmpObject = this.GetComponent<TextMeshProUGUI>();
+
+        // format seconds, fill with preceding zeros if the seconds are smaller than 10
+        string secondsStr = seconds.ToString();
+        while (secondsStr.Length < 2) secondsStr = "0" + secondsStr;
+
+        // update text content
+        tmpObject.SetText(string.Format("{0}:{1}", minutes, secondsStr));
+
+        // set text color to red if the seconds are less than 10
+        if (minutes < 1 && seconds < 10)
+        {
+            tmpObject.color = new Color(255, 0, 0);
+        } else
+        {
+            tmpObject.color = new Color(255, 255, 255);
+        }
+    }
+}
